7f360c30783cb5d692188c01fcccb6210a362b85
ayushpatwal011 <patwalayush2005@gmail.com>
2024-10-19 20:51:37 +0530

    create,update,get api

--- backend/controller/todo.controller.js
+++ backend/controller/todo.controller.js
@@ -1,6 +1,6 @@
 import Todo from "../model/todo.model.js";
 
-export const createTodo = async (req,res)=>{
+const createTodo = async (req,res)=>{
     const todo = new Todo({
         text: req.body.text,
         completed: req.body.completed
@@ -9,6 +9,31 @@ export const createTodo = async (req,res)=>{
         const newTodo = await todo.save();
         res.status(201).json({message:"Todo is created Successfully",newTodo});
     } catch (error) {
+        console.log(error)
         res.status(400).json({message:"Error in creating Todo"});
     }
 }
+const getTodos = async (req,res)=>{
+    try{
+        const todos = await Todo.find()
+        res.status(201).json({message:"Todo is created Successfully",todos});
+    }
+    catch(error){
+        console.log(error)
+        res.status(400).json({message:"Error in creating Todo"});
+    }
+}
+const updateTodo = async (req,res)=>{
+    try{
+        const updateTodo = await Todo.findByIdAndUpdate(req.params.id , req.body , {
+            new:true
+        })
+        res.status(201).json({message:"Todo is updated Successfully",updateTodo});
+    }
+    catch(error){
+        console.log(error)
+        res.status(400).json({message:"Error in updating Todo"});
+    }
+}
+
+export {createTodo , getTodos , updateTodo }
--- backend/routes/todo.route.js
+++ backend/routes/todo.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createTodo } from "../controller/todo.controller.js";
+import { createTodo , getTodos , updateTodo } from "../controller/todo.controller.js";
 
 const router = express.Router()
 
 router.post("/create",createTodo)
+router.get("/featch",getTodos)
+router.put("/update/:id",updateTodo)
 
 export default router
